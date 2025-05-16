cd12cd3ec8f65bb01dd52a11f954395a7a804451
elena130 <catanzaro.elena01@gmail.com>
2025-05-16 10:37:22 +0200

    Calculate duration of execution

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <chrono>
 #include "parser.h"
 #include "setcover.h"
 
 int main(int argc, char* argv[]) {
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
     std::fstream file;
     std::string line;
     unsigned int nr, nc;
@@ -68,6 +70,9 @@ int main(int argc, char* argv[]) {
     std::cout << std::endl;
 
     std::cout << "Solution cost: " << sc.solution_value(original_sc) << std::endl;
+
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
     
     return 0;
 }
