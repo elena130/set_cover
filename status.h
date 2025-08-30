2b1239aaa48cb63d6c08efb96ec7664e843b2f9f
elena130 <catanzaro.elena01@gmail.com>
2025-08-30 08:46:47 +0200

    Print enum values

--- status.h
+++ status.h
@@ -4,7 +4,18 @@ enum Status {
     FREE = -1,
     FIX_OUT = 0,
     FIX_IN = 1
+
 };
+
+
+inline std::ostream& operator<<(std::ostream& os, Status e) {
+    switch (e) {
+    case FIX_IN:  return os << "FIX_IN";
+    case FIX_OUT: return os << "FIX_OUT";
+    case FREE: return os << "FREE";
+    default:      return os << "UNKNOWN";
+    }
+}
 #endif 
 
 
